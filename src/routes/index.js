380f8dc98b495069d90525b17a24106b5b0e2e33
Sofia7323 <dsortizl@unadvirtual.edu.co>
2021-10-26 00:29:02 -0500

    Final

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('https://whispering-tundra-94459.herokuapp.com/about/', (req, res) => {
+router.get('/', (req, res) => {
   res.render('index');
 });
 
